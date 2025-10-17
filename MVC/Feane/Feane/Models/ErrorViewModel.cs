15461f5320bdb40c1ad4c4d04b62a9910cbfc7e8
tranmanhlinh <tranmanhlinh204@gmail.com>
2025-10-17 23:32:02 +0700

    change navbar and create MVC project

---
+++ MVC/Feane/Feane/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Feane.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
